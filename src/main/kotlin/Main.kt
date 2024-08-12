3185b8486b9f80f6ac8192985d29677fcf78d376
jinsang <jinsang@bugs.co.kr>
2024-08-12 15:46:13 +0900

    scheduler 관련 메서드 추가

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
 package org.example
 
 import java.math.BigInteger
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 val fibonacci: Sequence<BigInteger> = sequence {
@@ -40,6 +43,31 @@ fun randomUniqueStrings(
     }
 }.distinct()
 
+fun launchScheduler(
+    initDelay: Long = 3000,
+    delay: Long = 3000
+): ScheduledExecutorService {
+    val scheduler = Executors.newSingleThreadScheduledExecutor()
+    scheduler.scheduleWithFixedDelay({
+        println("!!!")
+    }, initDelay, delay, TimeUnit.MILLISECONDS)
+
+    return scheduler
+}
+
+fun shutDownSchedule(
+    delay: Long = 9000,
+    scheduler: ScheduledExecutorService
+) {
+    val _scheduler = Executors.newSingleThreadScheduledExecutor()
+    _scheduler.schedule({
+        println("try scheduler shutdown")
+        scheduler.shutdown()
+        println("try _scheduler shutdown")
+        _scheduler.shutdown()
+    }, delay, TimeUnit.MILLISECONDS)
+}
+
 fun main() {
     println(fibonacci.take(10).toList())
     println(randomNumbers(min = 1, max = 46).take(10).toList())
@@ -47,4 +75,7 @@ fun main() {
     println(it.next())
     println(it.next())
     println(it.next())
+
+    val scheduler = launchScheduler(initDelay = 0, delay = 2000)
+    shutDownSchedule(delay = 10000, scheduler = scheduler)
 }
